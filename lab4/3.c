f89749f8c2f228a7b3937773cb97b6bc38f525c5
aokigahara <nata.almukhametova.91@gmail.com>
2025-11-24 17:59:47 +0500

    Добавил возможность выбора размера

--- lab4/3.c
+++ lab4/3.c
@@ -6,12 +6,17 @@
 // Дан двумерный числовой массив. Значения элементов главной диагонали возвести в квадрат.
 
 int main() {
+    int size;
+
+    printf("Выберите размерность массива: ");
+    scanf("%d", &size);
+    
     srand(time(NULL));
-    int list[3][3]; // Выбрал двумерный за основу массив 3x3
+    int list[size][size];
     
     // Генератор чисел в массиве
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
             list[i][j] = rand() % 10 + 1;
             printf("%d ", list[i][j]);
         }
@@ -19,13 +24,13 @@ int main() {
     }
     
     // Цикл для возведения главной диагонали в квадрат
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < size; i++) {
         list[i][i] = pow(list[i][i], 2);
     }
     
     printf("\nПосле возведения в квадрат:\n");
-    for(int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
+    for(int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
             printf("%d ", list[i][j]);
         }
         printf("\n");
